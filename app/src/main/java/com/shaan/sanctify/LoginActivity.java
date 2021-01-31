61ef339530badf6173651b04b137e25178e15b45
SHAANRAASTI <shanrasti1@gmail.com>
2021-01-31 14:28:59 +0530

    BottomNavigation with DashboardActivity Implemented Successfully

--- app/src/main/java/com/shaan/sanctify/LoginActivity.java
+++ app/src/main/java/com/shaan/sanctify/LoginActivity.java
@@ -10,7 +10,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +32,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 100 ;
@@ -237,7 +240,7 @@ public class LoginActivity extends AppCompatActivity {
                             // sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
                             //user is logged in, so start LoginActivity
-                            startActivity(new Intent(LoginActivity.this,ProfileActivity.class));
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
                             finish();
 
                         }
@@ -295,10 +298,30 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
+                            //get user email and uid from auth
+                            String email = user.getEmail();
+                            String uid = user.getUid();
+                            // when user is registered store user info in firebase realtime database too
+                            //using Hashmap
+                            HashMap<Object, String> hashMap = new HashMap<>();
+                            //put  info in hashmap
+                            hashMap.put("email", email);
+                            hashMap.put("uid", uid);
+                            hashMap.put("namel", "");
+                            hashMap.put("phone", "");
+                            hashMap.put("image", "");
+                            //firebase database 9instance
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            //path to store data named "Users"
+                            DatabaseReference reference = database.getReference("Users");
+                            //put data within hashmap in database
+                            reference.child(uid).setValue(hashMap);
+
+
                             //show user email in toast
                             Toast.makeText(LoginActivity.this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
                             //got to ProfileActivity
-                            startActivity(new Intent(LoginActivity.this,ProfileActivity.class));
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
                             finish();
 
                         } else {
