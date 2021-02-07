fb0aa523773c3b3a05213b823a9ca90a022eee43
SHAANRAASTI <shanrasti1@gmail.com>
2021-02-07 09:53:28 +0530

    ImageActivity :- display im,age in full view is Implemented Successfully

--- app/src/main/java/com/shaan/sanctify/ImageActivity.java
+++ app/src/main/java/com/shaan/sanctify/ImageActivity.java
@@ -14,10 +14,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -25,6 +32,10 @@ public class ImageActivity extends AppCompatActivity {
     TextView textView;
     Button btnEdit,btnDel;
     DocumentReference reference;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseUser user;
+    FirebaseAuth firebaseAuth;
     String url;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -41,7 +52,10 @@ public class ImageActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String currentid = user.getUid();
 
-        reference = db.collection("user").document(currentid);
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("All Users");
+        //reference = db.collection("user").document(currentid);
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,21 +75,30 @@ public class ImageActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        String userid = user.getUid();
 
-                if (task.getResult().exists()){
-                    String name = task.getResult().getString("name");
-                    url = task.getResult().getString("url");
+        databaseReference.child(userid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NotNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    url = dataSnapshot.child("url").getValue().toString();
 
                     Picasso.get().load(url).into(imageView);
                     textView.setText(name);
-                }else {
+                } else {
                     Toast.makeText(ImageActivity.this, "No profile", Toast.LENGTH_SHORT).show();
                 }
             }
-        });
-    }
 
-}
+
+        @Override
+        public void onCancelled (@NonNull DatabaseError error){
+
+        }
+    });
+
+}}
